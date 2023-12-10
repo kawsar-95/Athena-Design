e98886512cb46165bb9a4cecd1bbc29b5cef6f6e
Nuruddin Kawsar <66555208+kawsar-95@users.noreply.github.com>
2023-12-11 02:17:02 +0600

    man trade international initiated

--- src/Components/Home/Showcase/Showcase.js
+++ src/Components/Home/Showcase/Showcase.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import './Showcase.css';
-import showcaseImg from '../../../Images/16 [Converted]@2x.png'; 
+import showcaseImg from '../../../Images/16 [Converted]@2x.png';
 
 const Showcase = () => {
-    return (
-        <div className="Showcase">
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-12 col-md-6 col-lg-6 showcaseDescription">
-                        <h1 className="showcaseTitle mb-3">Florence agency</h1>
-                        <p className="mb-4 text-muted">Lorem Ipsum has been the industry's standard dummy text ever
-                            since the 1500s, when an unknown printer took a galley of type and
-                            scrambled it to make a type specimen book.
-                        </p>
-                        <button className="pricingBtn contactBtn">See Pricing</button>
-                    </div>
-                    <div className="col-sm-12 col-md-6 col-lg-6">
-                        <img className="showcaseImg" src={showcaseImg} alt="showcaseImg"/>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="Showcase">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-md-6 col-lg-6 showcaseDescription">
+            <h1 className="showcaseTitle mb-3">MAN Trade International</h1>
+            <p className="mb-4 text-muted">
+              MAN Trade International is a dynamic and globally-oriented business agency that excels in facilitating international trade partnerships. Specializing in connecting businesses across borders, MAN Trade International serves as a strategic intermediary, fostering collaboration between suppliers, manufacturers, and distributors worldwide. The agency boasts a comprehensive understanding of international markets, trade regulations, and cultural nuances, enabling it to navigate the complexities of cross-border transactions with ease. With a focus on diverse industries, MAN Trade International offers a range of services, including market research, supply chain management, and logistical support. By leveraging its extensive network and expertise, the agency plays a crucial role in optimizing trade processes, reducing barriers, and enhancing overall efficiency for its clients. In an era where global business connections are paramount, MAN Trade International stands out as a trusted partner, facilitating seamless international trade and contributing to the growth and success of businesses on a global scale.
+            </p>
+            <button className="pricingBtn contactBtn">See Pricing</button>
+          </div>
+          <div className="col-sm-12 col-md-6 col-lg-6">
+            <img className="showcaseImg" src={showcaseImg} alt="showcaseImg" />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Showcase;
